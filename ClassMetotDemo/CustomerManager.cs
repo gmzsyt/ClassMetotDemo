ed7b36ba060801a8b6d431ecc1e4b73ca9f217a5
Gamze Seyit <gmzzsyyt@gmail.com>
2021-10-29 20:04:11 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void MusteriEkleme(Customer customer)
+        {
+            Console.WriteLine(customer.Ad + " " +"isimli yeni bir müşteri eklendi.");
+        }
+
+        public void MusteriSilme(Customer customer)
+        {
+            Console.WriteLine(customer.Ad  + " \t isimli müşteri silindi.");
+        }
+
+
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            // List<Customer> customer = new List<Customer>();
+            // customer.Add();
+
+            Customer customer1 = new Customer();
+            customer1.Ad = "Mustafa";
+            customer1.Soyad = "seyit";
+            customer1.Id =1;
+
+            Customer customer2 = new Customer();
+            customer2.Ad = "Gaye";
+            customer2.Soyad = "Seyit";
+            customer2.Id = 12;
+
+            Customer customer3 = new Customer();
+            customer3.Ad = "Ahmet";
+            customer3.Soyad = "seyit";
+            customer3.Id = 15;
+
+
+            CustomerManager met = new CustomerManager();
+            met.MusteriSilme(customer2);
+
+
+
+        }
+    }
+}
